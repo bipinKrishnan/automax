1bc84dbecb7b6102e72a32513d87b865056d3ba4
bipin <bipinkrishna.p@gmail.com>
2021-09-05 14:26:42 +0530

    added exception handling

--- centroid/experiments.py
+++ centroid/experiments.py
@@ -1,11 +1,37 @@
 import streamlit as st
 import streamlit.components.v1 as components
+
 import os 
+from functools import partial
+
+def show_plots(
+    col,
+    dir_name, 
+    selected_ipynb,
+    load_cache
+    ):
+        with col:
+            ipynb_path = os.path.join(dir_name, selected_ipynb)
+            html_name = selected_ipynb.split('.')[0]
+
+            if not load_cache:
+                os.system(
+                    f"jupyter nbconvert --to html --no-input {ipynb_path} --output {html_name}.html"
+                    )
+
+            try:        
+                with open(os.path.join(dir_name, f"{html_name}.html")) as f:
+                    components.html(
+                        f.read(),
+                        height=500,
+                        scrolling=True
+                        )
+            except FileNotFoundError:
+                st.error(f"No cache found for {selected_ipynb}")
 
 def display_ipynb_plots():
     cwd = os.getcwd()
     dir_name = 'experiments'
-    default_option = 'None'
     col1, col2 = st.columns([1, 3])
 
     ipynb_files = [dir for dir in os.listdir(dir_name) if dir.split('.')[-1]=='ipynb']
@@ -14,26 +40,21 @@ def display_ipynb_plots():
         with col1:
             selected_ipynb = st.selectbox(
                     label="Select ipynb files to view plots",
-                    options=[default_option]+ipynb_files
+                    options=ipynb_files
                 )
-            load_cache = st.checkbox("Load from cache", value=True)
 
-        with col2:
-            if selected_ipynb!=default_option:
-                ipynb_path = os.path.join(dir_name, selected_ipynb)
-                html_name = selected_ipynb.split('.')[0]
-
-                if not load_cache:
-                    os.system(
-                        f"jupyter nbconvert --to html --no-input {ipynb_path} --output {html_name}.html"
-                        )
-                        
-                with open(os.path.join(dir_name, f"{html_name}.html")) as f:
-                    components.html(
-                        f.read(),
-                        height=500,
-                        scrolling=True
-                        )
+            if selected_ipynb:
+                load_cache = st.checkbox("Load from cache", value=True)
+                st.button(
+                    'Show plots',
+                    on_click=partial(
+                        show_plots,
+                        col=col2,
+                        dir_name=dir_name, 
+                        selected_ipynb=selected_ipynb,
+                        load_cache=load_cache
+                    )
+                )  
             
     else:
         st.error("⚠️ No Jypter notebooks 📓 found in experiments folder")
